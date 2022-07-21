36ca62e8ca6b7d2346c5e4a1865854dbd24a6bf5
valentesteban <valente.estebann@gmail.com>
2022-07-20 21:47:53 -0300

    feat: add chat utility

---
+++ api/src/main/java/am/esteban/lobby/api/translate/ChatUtil.java
@@ -0,0 +1,35 @@
+package am.esteban.lobby.api.translate;
+
+import org.bukkit.ChatColor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatUtil {
+
+    public static String translate(String in) {
+        return ChatColor.translateAlternateColorCodes('&', in);
+    }
+
+    public static List<String> translate(List<String> lines) {
+        List<String> toReturn = new ArrayList<>();
+
+        for (String line : lines) {
+            toReturn.add(ChatColor.translateAlternateColorCodes('&', line));
+        }
+
+        return toReturn;
+    }
+
+    public static List<String> translate(String[] lines) {
+        List<String> toReturn = new ArrayList<>();
+
+        for (String line : lines) {
+            if (line != null) {
+                toReturn.add(ChatColor.translateAlternateColorCodes('&', line));
+            }
+        }
+
+        return toReturn;
+    }
+}
